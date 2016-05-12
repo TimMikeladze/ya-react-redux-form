445ce38f3d7d078c5eada285b359df520d72366c
Tim Mikeladze <tim.mikeladze@gmail.com>
2016-05-11 19:22:55 -0700

    Finished submit method

--- src/yaForm.js
+++ src/yaForm.js
@@ -12,41 +12,76 @@ const YaForm = {
     YaForm.getState = store.getState;
     YaForm.dispatch = store.dispatch;
   },
-  submit({ formName, validator, schema, promise, onSubmit, onSuccess, onFailure, onValidation }) {
-    if (onSubmit !== false) {
-      const formValidator = validator || YaForm.config.validator;
+  submit({ formName, validator, schema, method, onSubmit, onSuccess, onFailure, onValidation }) {
+    // Use the configured validator if no validator provided
+    const formValidator = validator || YaForm.config.validator;
 
-      if (!formValidator) {
-        throw new Error('A validator must be provided or set as default.');
-      }
-      if (!schema) {
-        throw new Error('A schema must be provided.');
-      }
-      if (!(promise instanceof Promise)) {
-        // throw new Error('A promise must be provided');
-      }
+    // Validate required arguments
+    if (!formValidator) {
+      throw new Error('A `validator` key must be provided or set as default.');
+    }
+    if (!schema) {
+      throw new Error('`schema` key must be provided.');
+    }
 
-      const form = (() => {
-        const formState = YaForm.getState().yaForm[formName];
-        const obj = {};
-        if (formState.hasOwnProperty('fields')) {
-          const fields = formState.fields;
-          for (const key in fields) {
-            if (key) {
-              obj[key] = fields[key].value;
-            }
+    // Create an object from the current state containg a mapping between field names and values.
+    const form = (() => {
+      const formState = YaForm.getState().yaForm[formName];
+      const obj = {};
+      if (formState.hasOwnProperty('fields')) {
+        const fields = formState.fields;
+        for (const key in fields) {
+          if (key) {
+            obj[key] = fields[key].value;
           }
         }
-        return obj;
-      })();
+      }
+      return obj;
+    })();
 
-      if (onSubmit) onSubmit(formName, form);
+    // Run onSubmit callback
+    if (onSubmit) {
+      onSubmit({ formName, form, dispatch: YaForm.dispatch, getState: YaForm.getState });
+    }
 
-      const validation = formValidator(form, schema, YaForm.dispatch, YaForm.getState);
-      if (onValidation) onValidation(formName, form, validation);
+    // Validate the form. formValidator returns undefined if there are no validation errors.
+    const validationResults = formValidator(
+      { formName, form, schema, dispatch: YaForm.dispatch, getState: YaForm.getState }
+    );
+    // Run onValidation callback
+    if (onValidation) {
+      onValidation(
+        { formName, form, validationResults, dispatch: YaForm.dispatch, getState: YaForm.getState }
+      );
+    }
 
-      if (!validation) {
-        // TODO How to handle a promise? Accept only promises or regular methods too? Throw exception?
+    // If validation passed run provided method
+    if (!validationResults) {
+      if (method instanceof Function) {
+        // Run the method
+        const promise = method(
+          { formName, form, dispatch: YaForm.dispatch, getState: YaForm.getState }
+        );
+        // Require method to return a Promise
+        if (!(promise instanceof Promise)) {
+          throw new Error('method must return a Promise');
+        }
+        // Handle promise
+        promise.then((result) => {
+          // Run onSuccess callback
+          if (onSuccess) {
+            onSuccess(
+              { result, formName, form, dispatch: YaForm.dispatch, getState: YaForm.getState }
+            );
+          }
+        }).catch((err) => {
+          // Run onFailure callback
+          if (onFailure) {
+            onFailure(
+              { err, formName, form, dispatch: YaForm.dispatch, getState: YaForm.getState }
+            );
+          }
+        });
       }
     }
   },
