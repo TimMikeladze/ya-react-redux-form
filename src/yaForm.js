28504308e401c45adb1165c05463d7008c4ac922
Tim Mikeladze <tim.mikeladze@gmail.com>
2016-05-13 01:20:07 -0700

    Pass more arguments to promise callbacks

--- src/yaForm.js
+++ src/yaForm.js
@@ -12,22 +12,19 @@ const YaForm = {
     YaForm.getState = store.getState;
     YaForm.dispatch = store.dispatch;
   },
-  submit({ formName, validator, schema, method, onSubmit, onSuccess, onFailure, onValidation }) {
+  submit({ name, validator, schema, method, onSubmit, onSuccess, onFailure, onValidation }) {
     const promise = new Promise((resolve, reject) => {
       // Use the configured validator if no validator provided
       const formValidator = validator || YaForm.config.validator;
 
       // Validate required arguments
       if (!formValidator) {
-        reject('A `validator` key must be provided or set as default.');
-      }
-      if (!schema) {
-        reject('`schema` key must be provided.');
+        // reject('A `validator` key must be provided or set as default.');
       }
 
       // Create an object from the current state containg a mapping between field names and values.
       const form = (() => {
-        const formState = YaForm.getState().yaForm[formName];
+        const formState = YaForm.getState().yaForm[name];
         const obj = {};
         if (formState.hasOwnProperty('fields')) {
           const fields = formState.fields;
@@ -42,17 +39,20 @@ const YaForm = {
 
       // Run onSubmit callback
       if (onSubmit) {
-        onSubmit({ formName, form, dispatch: YaForm.dispatch, getState: YaForm.getState });
+        onSubmit({ name, form, dispatch: YaForm.dispatch, getState: YaForm.getState });
       }
 
       // Validate the form. formValidator returns undefined if there are no validation errors.
-      const validationResults = formValidator(
-        { formName, form, schema, dispatch: YaForm.dispatch, getState: YaForm.getState }
-      );
+      let validationResults;
+      if (formValidator) {
+        validationResults = formValidator(
+          { name, form, schema, dispatch: YaForm.dispatch, getState: YaForm.getState }
+        );
+      }
       // Run onValidation callback
       if (onValidation) {
         onValidation(
-          { formName, form, validationResults,
+          { name, form, validationResults,
             dispatch: YaForm.dispatch, getState: YaForm.getState }
         );
       }
@@ -62,7 +62,7 @@ const YaForm = {
         if (method instanceof Function) {
           // Run the method
           const methodPromise = method(
-            { formName, form, dispatch: YaForm.dispatch, getState: YaForm.getState }
+            { name, form, dispatch: YaForm.dispatch, getState: YaForm.getState }
           );
           // Require method to return a Promise
           if (!(methodPromise instanceof Promise)) {
@@ -73,24 +73,28 @@ const YaForm = {
             // Run onSuccess callback
             if (onSuccess) {
               onSuccess(
-                { result, formName, form, dispatch: YaForm.dispatch, getState: YaForm.getState }
+                { result, name, form, dispatch: YaForm.dispatch, getState: YaForm.getState }
               );
             }
-            resolve(result);
+            resolve({ result, name, form, dispatch: YaForm.dispatch, getState: YaForm.getState });
           }).catch(err => {
             // Run onFailure callback
             if (onFailure) {
               onFailure(
-                { err, formName, form, dispatch: YaForm.dispatch, getState: YaForm.getState }
+                { err, name, form, dispatch: YaForm.dispatch, getState: YaForm.getState }
               );
             }
-            reject(err);
+            reject({ err, name, form, dispatch: YaForm.dispatch, getState: YaForm.getState });
           });
         } else {
-          resolve();
+          resolve(
+            { name, form, dispatch: YaForm.dispatch, getState: YaForm.getState }
+          );
         }
       } else {
-        reject(validationResults);
+        reject(
+          { validationResults, name, form, dispatch: YaForm.dispatch, getState: YaForm.getState }
+        );
       }
     });
     return promise;
