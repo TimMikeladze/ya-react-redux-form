ea633860b2ef5b6fd8a120e02f7438ec7b85ef6d
Tim Mikeladze <tim.mikeladze@gmail.com>
2016-05-13 03:35:32 -0700

    Added actions for errors and field invalidation

--- src/redux/modules.js
+++ src/redux/modules.js
@@ -4,9 +4,26 @@ const CREATE_FIELD = 'ya-react-form/CREATE_FIELD';
 const REMOVE_FIELD = 'ya-react-form/REMOVE_FIELD';
 const CHANGE_FIELD_VALUE = 'ya-react-form/CHANGE_FIELD_VALUE';
 const SUBMIT_FORM = 'ya-react-form/SUBMIT_FORM';
+const ERROR = 'ya-react-form/ERROR';
+const INVALIDATED_FIELD = 'ya-react-form/INVALIDATED_FIELD';
 
 const reducer = (state = {}, action = {}) => {
   switch (action.type) {
+    case SUBMIT_FORM: {
+      const nextState = objectAssignDeep({}, state);
+      // Clear form level error
+      if (nextState[action.formName].hasOwnProperty('error')) {
+        delete nextState[action.formName].error;
+      }
+      // Clear field level errors
+      nextState[action.formName] = nextState[action.formName].fields.forEach(field => (
+        {
+          value: field.value,
+          hasError: false,
+        }
+      ));
+      return nextState;
+    }
     case CHANGE_FIELD_VALUE:
       return objectAssignDeep({}, state, {
         [action.formName]: {
@@ -17,8 +34,6 @@ const reducer = (state = {}, action = {}) => {
           },
         },
       });
-    case SUBMIT_FORM:
-      return state;
     case REMOVE_FIELD: {
       const nextState = objectAssignDeep({}, state);
       delete nextState[action.formName].fields[action.name];
@@ -33,6 +48,24 @@ const reducer = (state = {}, action = {}) => {
           fields: {
             [action.name]: {
               value: action.value,
+              hasError: false,
+            },
+          },
+        },
+      });
+    case ERROR:
+      return objectAssignDeep({}, state, {
+        [action.formName]: {
+          error: action.reason,
+        },
+      });
+    case INVALIDATED_FIELD:
+      return objectAssignDeep({}, state, {
+        [action.formName]: {
+          fields: {
+            [action.name]: {
+              error: action.reason,
+              hasError: true,
             },
           },
         },
@@ -75,7 +108,7 @@ const changeFieldValue = ({ formName, name, value }) => (
 );
 export { changeFieldValue };
 
-const submitForm = ({ formName, simpleSchema, options = {} }) => (dispatch, getState) => {
+const submitForm = ({ formName }) => (dispatch) => {
   dispatch({
     type: SUBMIT_FORM,
     formName,
@@ -83,3 +116,24 @@ const submitForm = ({ formName, simpleSchema, options = {} }) => (dispatch, getS
 };
 
 export { submitForm };
+
+const error = ({ formName, reason }) => (
+  {
+    type: ERROR,
+    formName,
+    reason,
+  }
+);
+
+export { error };
+
+const invalidateField = ({ formName, name, reason }) => (
+  {
+    type: INVALIDATED_FIELD,
+    formName,
+    name,
+    reason,
+  }
+);
+
+export { invalidateField };
