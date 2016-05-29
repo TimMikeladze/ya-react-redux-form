2c4e3686d63bf1d203d575cada1a3cca19bd0c7e
Tim Mikeladze <tim.mikeladze@gmail.com>
2016-05-28 23:24:13 -0700

    Remove redux-logger

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,16 +1,13 @@
 import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
-import createLogger from 'redux-logger';
 
 import yaForm from './modules';
-
-const logger = createLogger();
 
 const reducers = combineReducers({ yaForm });
 
 const configureStore = (initialState = {}) =>
   createStore(reducers, initialState, compose(
-      applyMiddleware(thunk, logger)
+      applyMiddleware(thunk)
   ));
 
 export default configureStore;
