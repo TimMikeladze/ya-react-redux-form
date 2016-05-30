73b31f01cc4d2b02eb76393bc58ab9e69bce3836
Tim Mikeladze <tim.mikeladze@gmail.com>
2016-05-30 03:20:33 -0700

    stubbed ya form tests

--- test/YaForm.ts
+++ test/YaForm.ts
@@ -3,5 +3,23 @@ import chaiAsPromised from 'chai-as-promised';
 import YaForm from '../src/YaForm';
 
 describe('YaForm', () => {
+  it('can be provided a store', () => {
 
+  });
+
+  it('submit expects the form to be in the store', () => {
+
+  });
+
+  describe('onSubmit', () => {
+
+  });
+
+  describe('calls validator', () => {
+
+  });
+
+  describe('calls method', () => {
+
+  });
 });
