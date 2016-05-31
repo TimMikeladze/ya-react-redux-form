de63a9bb94b49d9021079efeacbec807c0940fa3
Tim Mikeladze <tim.mikeladze@gmail.com>
2016-05-30 23:25:15 -0700

    Added React

--- typings/browser/index.d.ts
+++ typings/browser/index.d.ts
@@ -3,5 +3,7 @@
 /// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="globals/promises-a-plus/index.d.ts" />
+/// <reference path="globals/react-dom/index.d.ts" />
+/// <reference path="globals/react/index.d.ts" />
 /// <reference path="globals/redux-thunk/index.d.ts" />
 /// <reference path="globals/redux/index.d.ts" />
